5a7c7c1745d7baa3b835b2e89552dfb23a193f0c
Fncampos <flaviancampos@hotmail.com>
2023-04-03 20:43:43 -0300

    Comunicaçao basica

---
+++ ClassificadosCliente/src/main/java/com/mycompany/classificadoscliente/ClassificadosVeiculo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.classificadoscliente;
+import java.rmi.RemoteException;
+import java.rmi.Remote;
+/**
+ *
+ * @author flavi
+ */
+public interface ClassificadosVeiculo extends Remote {
+    public String exibirMensagem(String txt) throws RemoteException;
+    
+}
+
